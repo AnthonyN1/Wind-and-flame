be1a441a997a6294204a306e682187f98f574a9d
AnthonyN1 <anthonyjnool@gmail.com>
2022-12-23 19:51:21 -0500

    Revise deploy formatting

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
 # Pulls any updates from the remote repository.
-printf "W&F: Pulling from repository...\n"
+echo "W&F: Pulling from repository..."
 git pull
-printf "==================================================\n\n"
 
 # If a virtual environment doesn't exist, creates one.
 if [ ! -d ".venv" ]; then
-	printf "W&F: Creating virtual environment...\n"
+	echo "W&F: Creating virtual environment..."
 	python3 -m venv .venv
-	printf "==================================================\n\n"
 fi
 
 # Activates the virtual environment.
-printf "W&F: Activating virtual environment...\n"
+echo "W&F: Activating virtual environment..."
 source .venv/bin/activate
-printf "==================================================\n\n"
 
 # Installs required Python packages.
-printf "W&F: Installing packages...\n"
+echo "W&F: Installing packages..."
 pip3 install -r requirements.txt | grep -v "already satisfied"
-printf "==================================================\n"
+
