9b3d2fcf51696a5d1c04d83ca9c97b6e3fbec4ad
AnthonyN1 <anthonyjnool@gmail.com>
2023-01-02 00:31:09 -0500

    Implement the start of polling

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from logger import bot_logger
 import nextcord
 from nextcord.ext import commands
 import sys
+import time
 
 
 # Initialize the bot.
@@ -19,6 +20,63 @@ except decouple.UndefinedValueError as e:
 @bot.event
 async def on_ready():
 	bot_logger.info("The bot has successfully logged in.")
+
+
+@bot.slash_command()
+async def poll(
+	interaction: nextcord.Interaction, 
+	question: str, 
+	choices: str, 
+	length: int, 
+	color: int = nextcord.SlashOption(
+		choices={
+			"red": nextcord.Color.red().value, "orange": nextcord.Color.orange().value, "yellow": nextcord.Color.yellow().value, 
+			"green": nextcord.Color.green().value, "blue": nextcord.Color.blue().value, "purple": nextcord.Color.purple().value, 
+			"gray": nextcord.Color.light_gray().value
+		}
+	)
+):
+	"""
+		This is the main description.
+
+		Parameters
+		=========================
+		interaction: Interaction
+			The interaction object.
+		question: str
+			The poll question.
+		choices: str
+			A command-separated string of poll choices.
+		length: int
+			The number of days the poll will be open for.
+		color: int
+			The color of the left side-bar.
+	"""
+	# The title of the embed.
+	embed_title = f"\U0001F4DD {question}"
+
+	# The description of the embed.
+	choices_list = [choice.strip() for choice in choices.split(",") if choice != ""]
+	if len(choices_list) < 2:
+		await interaction.send("You must specify at least two poll choices!", ephemeral=True)
+		return
+	if len(choices_list) >= 10:
+		await interaction.send("Sorry, polls can only have up to nine (9) choices.", ephemeral=True)
+		return
+	
+	embed_description = ""
+	for i in range(len(choices_list)):
+		embed_description += chr(i + 49) + f"\U000020E3 {choices_list[i]}\n"
+	embed_description += f"\nThis poll will close <t:{int(time.time()) + (length * 24 * 60 * 60)}:R>"
+
+	# Sends the poll to the channel.
+	embed_poll = nextcord.Embed(title=embed_title, description=embed_description, color=color)
+	partial_message = await interaction.send(embed=embed_poll)
+
+	# Reacts to the message with each choice number.
+	message = await partial_message.fetch()
+	for i in range(len(choices_list)):
+		await message.add_reaction(chr(i + 49) + "\U000020E3")
 
 
 bot.run(token)
